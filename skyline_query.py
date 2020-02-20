c79d79695d8bab0528c725718f5583413a83a462
sangmain <sangmin99554@naver.com>
2020-02-20 18:34:45 +0900

    fix errors

--- skyline_query.py
+++ skyline_query.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 ############################# 거리함수
-def euclidean_distance(x, y):
-    import math
+def euclidean_distance_2d(x, y):
     distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
     return distance
+
+def euclidean_distance(x,y):
+    n= x**2 + y**2
+    return math.sqrt(n)
 
 ############################# NN 인덱스 반환
 def boundedNNSearch(data, distance_function):
         
     data_distance = []
     for x, y in data:
-        data_distance.append(distance_function([0,0], [x,y])) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
+#         data_distance.append(distance_function([0,0], [x,y])) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
+        data_distance.append(distance_function(x, y)) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
 
     nn_idx = data_distance.index(min(data_distance)) # Nearest Neighbor의 인덱스를 얻는다
     return nn_idx
@@ -25,11 +30,12 @@ def data_cut(data, todo, nn, region_idx):
     for i in range(len(data)):
         x, y = data[i]
 
-        region_dict ={0: x >= nn[0] and y >= nn[1], 1: y < nn[1], 2: x < nn[0]} #Region 0인지 1인지 2인지 딕셔너리
+        region_dict ={0: x > nn[0] and y > nn[1], 1: y > nn[1], 2: x > nn[0]} #Region 0인지 1인지 2인지 딕셔너리
             
         if region_dict[region_idx]:
             continue
-        region.append(data[i])
+        if x != nn[0] and y != nn[1]:
+            region.append(data[i])
         
     if region_idx == 0:
         return region #Region의 데이터 반환
@@ -43,9 +49,9 @@ def data_cut(data, todo, nn, region_idx):
 ############################# 메인 함수
 def getskylines(data, distance_function=euclidean_distance):
     ######### 데이터셋 정규화
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-    data = scaler.fit_transform(data)
+#     from sklearn.preprocessing import MinMaxScaler
+#     scaler = MinMaxScaler()
+#     data = scaler.fit_transform(data)
     
     # norm_data = pd.DataFrame(norm_data, columns=data.columns)
     # norm_data.plot.scatter(x="price", y="distance")
@@ -53,6 +59,8 @@ def getskylines(data, distance_function=euclidean_distance):
 
     todo = []
     todo.append(data) # Todo에 데이터 추가
+#     b = data
+#     b.sort()
     del data
 
     skylines = []
@@ -66,9 +74,13 @@ def getskylines(data, distance_function=euclidean_distance):
         data_cut(data, todo, nn, 2) # Region 2 todo에 추가
 
         
-        nn = scaler.inverse_transform([nn]) #역정규화
-        skylines.append(nn.reshape(2).tolist()) #스카이라인 추가
-        print(skylines)
+#         nn = scaler.inverse_transform([nn]) #역정규화
+        skylines.append(nn) #스카이라인 추가
+#         skylines.append(nn.reshape(2).tolist()) #스카이라인 추가
+        print(nn)
+    
+#         if i == 3:
+#             return skylines
 
         todo[i] = None # 필요없는 변수 없애기
 
@@ -84,4 +96,4 @@ def getskylines(data, distance_function=euclidean_distance):
 # plt.xlim(0.34752048913785316,  190.65247951086215) 
 # plt.ylim(-0.06839386840927793, 2.0993498601356593)
 # plt.savefig("Region")
-# plt.show()
+# plt.show(
--- skyline_query2.py
+++ skyline_query2.py
@@ -2,17 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ############################# 거리함수
-def euclidean_distance(x, y):
+def euclidean_distance_2d(x, y):
     import math
     distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
     return distance
+
+def euclidean_distance(x,y):
+    n= x**2 + y**2
+    return math.sqrt(n)
+
 
 ############################# NN 인덱스 반환
 def boundedNNSearch(data, distance_function):
         
     data_distance = []
     for x, y in data:
-        data_distance.append(distance_function([0,0], [x,y])) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
+#         data_distance.append(distance_function([0,0], [x,y])) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
+        data_distance.append(distance_function(x, y)) # 원점과 현재 데이터의 거리 계산 후 data_distance에 append한다
 
     # nn_idx = data_distance.index(min(data_distance)) # Nearest Neighbor의 인덱스를 얻는다
     return data_distance
@@ -21,36 +27,35 @@ def boundedNNSearch(data, distance_function):
 ############################# 메인 함수
 def getskylines(data, distance_function=euclidean_distance):
     ######### 데이터셋 정규화
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-    data = scaler.fit_transform(data)
-    data = data.tolist()
+#     from sklearn.preprocessing import MinMaxScaler
+#     scaler = MinMaxScaler()
+#     data = scaler.fit_transform(data)
+#     data = data.tolist()
     skylines = []
 
     data_dist = boundedNNSearch(data, distance_function) # 데이터 전체에 대한 Distance 값을 반환 받는다
     _, data = zip(*sorted(zip(data_dist, data))) # 데이터를 Distance의 순서로 sorting
-    data = scaler.inverse_transform(data).tolist() #역정규화
+#     data = scaler.inverse_transform(data).tolist() #역정규화
 
     # 첫 NN
-    skylines.append(data[0]) 
-    data = data[1:]   
-    print(skylines)
+    skylines.append(data[0])
+    print(data[0])
 
-    index = 0
+    index = 1
     while True:
         temp_data = []
-        for i in range(len(data)):
+        for i in range(index, len(data), 1):
             x, y = data[i]
 
-            if x >= skylines[index][0] and y >= skylines[index][1]: # REGION 3면
+            if x >= data[index - 1][0] and y >=  data[index - 1][1]: # REGION 3면
                 continue
             temp_data.append(data[i])
-
+        
         data = temp_data
-        if len(data) == 0:
+        if len(data) == 0 :
             break
-        skylines.append(data[0])
-        print(data[0])
+        skylines.append(data[index])
+        print(data[index])
         index += 1
 
     
